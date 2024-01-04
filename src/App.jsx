40bf113e348149a4276c59785a342c37d360608c
Aniket-x-code <aniket.209301578@muj.manipal.edu>
2024-01-04 05:48:15 +0530

    adding dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
+import {BrowserRouter as Router, Switch, Route, BrowserRouter} from 'react-router-dom';
+import Dashboard from './pages/Dashboard/Dashboard';
+
 const App = () => {
-  return <span>Subscribe Zain keepscode</span>;
+  return <>
+  <div id="dashboard">
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Layout />} />
+      <Route path="dashboard" element={<Dashboard />} />
+    </Routes>
+    </BrowserRouter>
+  </div>
+  </>
 };
 
 export default App;
---
+++ src/pages/Dashboard/Dashboard.jsx
@@ -0,0 +1,11 @@
+import css from './Dashboard.module.css'
+
+const Dashboard = () => {
+  return (
+    <div>
+      Dashboard
+    </div>
+  )
+}
+
+export default Dashboard
