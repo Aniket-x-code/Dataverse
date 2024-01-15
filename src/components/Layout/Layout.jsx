be9ff552363fa8b14a2e25f498e4befffc3372d0
Aniket-x-code <aniket.209301578@muj.manipal.edu>
2024-01-15 20:50:19 +0530

    sidebar and topgrid base added

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -3,7 +3,18 @@ import css from './Layout.module.css'
 
 const Layout = () => {
   return ( <div className={css.container}>
-      
+      <div>Sidebar</div>
+
+
+      <div className={css.dashboard}>
+        <div className={css.topBaseGradients}>
+            <div className='gradient-red'></div>
+            <div className='gradient-orange'></div>
+            <div className='gradient-blue'></div>
+        </div>
+      </div>
+
+
     </div>
   )
 }
