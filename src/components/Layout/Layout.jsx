68c45314f9eab4a7c5edb220293bc6e6c9fddeda
Aniket-x-code <aniket.209301578@muj.manipal.edu>
2024-01-28 21:07:35 +0530

    sidebar

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -14,15 +14,21 @@ const Layout = () => {
             <div className='gradient-orange'></div>
             <div className='gradient-blue'></div>
         </div>
-        <div className="css.header">
-          <span>{moment().format("dddd,Do MMM YYYY")}</span>
+        <div className={css.header}>
+          <span>{moment().format("dddd, Do MMM YYYY")}</span>
 
-          <div className="css.searchBar">
+          <div className={css.searchBar}>
             <Bisearch size={20}/>
             <input type="text" placeholder='Search here' />
           </div>
 
-          <div className="css.profile"></div>
+          <div className={css.profile}>
+            <img src="./profile.png" alt="profile image" />
+            <div className={css.details}>
+                <span>Aniket</span>
+                <span>verma13aniket@gmail.com</span>
+            </div>
+          </div>
 
         </div>
       </div>
---
+++ src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,11 @@
+import css from './Sidebar.module.css'
+
+const Sidebar = () => {
+  return (
+    <div>
+      Sidebar
+    </div>
+  )
+}
+
+export default Sidebar
