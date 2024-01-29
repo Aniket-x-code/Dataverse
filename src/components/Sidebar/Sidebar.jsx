c42d408ba40e4364583d3d8a50bc65aaa6c535d3
Aniket-x-code <aniket.209301578@muj.manipal.edu>
2024-01-29 22:50:43 +0530

    updates of sidebar

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,11 +1,17 @@
 import css from './Sidebar.module.css'
+import { MdSpaceDashboard } from "react-icons/md";
+import { AiFillCalendar, AiOutlineTable } from "react-icons/ai";
+import { FaTasks } from "react-icons/fa";
 
 const Sidebar = () => {
   return (
-    <div>
-      Sidebar
+    <div className={css.container}>
+
+          <img src="./Logo.png" alt="logo" />
+
     </div>
   )
 }
 
 export default Sidebar
+
