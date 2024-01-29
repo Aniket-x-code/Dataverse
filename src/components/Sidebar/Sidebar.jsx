f0a768075730ba8ec1e26c118b3d2802b3050732
Aniket-x-code <aniket.209301578@muj.manipal.edu>
2024-01-29 22:57:43 +0530

    changes of sidebar

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -2,12 +2,17 @@ import css from './Sidebar.module.css'
 import { MdSpaceDashboard } from "react-icons/md";
 import { AiFillCalendar, AiOutlineTable } from "react-icons/ai";
 import { FaTasks } from "react-icons/fa";
+import { NavLink } from 'react-router-dom';
 
 const Sidebar = () => {
   return (
     <div className={css.container}>
 
-          <img src="./Logo.png" alt="logo" />
+          <img src="./Logo.png" alt="" />
+          <div className={css.menu}>
+            <NavLink to="dashboard" className={css.item} title={'Dashboard'} ></NavLink>
+
+          </div>
 
     </div>
   )
