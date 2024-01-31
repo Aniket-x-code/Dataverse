681a533954170d311ff3b162bfc0353fb0595bd4
Aniket-x-code <aniket.209301578@muj.manipal.edu>
2024-01-31 21:33:10 +0530

    fixing layout

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -2,8 +2,24 @@ import css from './Dashboard.module.css'
 
 const Dashboard = () => {
   return (
-    <div>
-      Dashboard
+    <div className={css.container}>
+      {/* left side  */}
+      <div className={css.dashboard}>
+          <div className={css.dashboardHead}>
+                <div className={css.head}>
+                  <span>Dahboard</span>
+
+                  <div className={css.duration}>
+
+                  </div>
+                  
+                </div>
+          </div>
+      </div>
+
+      <div className={css.orders}>
+        orders
+      </div>
     </div>
   )
 }
